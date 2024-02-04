02ef6fdba978da87ebfadf112b901a863c490c45
maikCyphlock <maikolaguilar656@gmail.com>
2024-02-03 20:04:50 -0400

    ✨ feat: add contentlayer for markdown blog

---
+++ contentlayer.config.js
@@ -0,0 +1,63 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+
+/**
+ * @type {import('contentlayer/source-files').ComputedFields}
+ */
+
+const computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+  },
+};
+
+export const Doc = defineDocumentType(() => ({
+  name: "Doc",
+  filePathPattern: `markdown/**/*.mdx`,
+  contentType: "mdx",
+
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+    published: {
+      type: "boolean",
+      default: true,
+    },
+    cover_url: {
+      type: "string",
+    },
+  },
+  computedFields,
+}));
+
+export default makeSource({
+  contentDirPath: "src/content",
+  documentTypes: [Doc],
+  disableImportAliasWarning: true,
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"],
+          },
+        },
+      ],
+    ],
+  },
+});
