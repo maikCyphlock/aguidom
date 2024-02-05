e41a7f7830623b19ddcb1e1a3a3087c6ee0e175c
maikCyphlock <maikolaguilar656@gmail.com>
2024-02-05 19:45:57 -0400

    💄 style: add beautiful tailwind button

--- src/components/userMenu.tsx
+++ src/components/userMenu.tsx
@@ -25,11 +25,17 @@ async function UserMenu() {
   if (!profiles || error) {
     return (
       <Button
-        className="  group transition-all duration-200 ease-in-out lg:flex border-[#1c1c1c] bg-[#151515]  justify-center  gap-2 cursor-pointer  items-center p-3 px-2 border rounded-2xl hover:bg-[#1c1c1c] hover:border-[#1c1c1c] tracking-[-0.24] text-[#c6c6c6] "
+        className="overflow-hidden rounded-3xl relative w-32 p-2 h-12 bg-black text-white border-none  text-lg font-bold cursor-pointer  z-10 group"
         asChild
       >
         <a href="/login" className="text-center   ">
-          Inicia Sesion
+          Inicia sesión
+          <span className="absolute w-36 h-32 -top-8 -left-2 bg-sky-200 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-right"></span>
+          <span className="absolute w-36 h-32 -top-8 -left-2 bg-sky-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-right"></span>
+          <span className="absolute w-36 h-32 -top-8 -left-2 bg-sky-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-right"></span>
+          <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-4 z-10">
+            bienvenido
+          </span>
         </a>
       </Button>
     );
