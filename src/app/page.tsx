d44432f8be01fa4ac6ef50bc0a5300b1817018c8
maikCyphlock <maikolaguilar656@gmail.com>
2024-02-09 21:04:59 -0400

    ✨ feat: add sponsor area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Experience from "@/components/Experience";
 import Win_case from "@/components/Win_case";
 import Plans from "@/components/Plans";
 import Pricing from "@/components/Pricing";
+import Sponsor from "@/components/Sponsor";
 
 export default function Home() {
   return (
     <div className="container mx-auto">
       <Cta />
+      <Sponsor />
       <div className="lg:px-20">
         <Experience />
         <Win_case />
