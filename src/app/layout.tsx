208aee6128975aec1eacff04f101326ce50e59b7
maikCyphlock <maikolaguilar656@gmail.com>
2024-02-12 19:46:51 -0400

    🔥 remove dead code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,17 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <head>
-        {(process.env.NODE_ENV === "development" ||
-          process.env.VERCEL_ENV === "preview") && (
-          // eslint-disable-next-line @next/next/no-sync-scripts
-          <script
-            data-project-id="umbd2yZ2605Th0nur4HdcLowH7MqczVKdx3ZLwAk"
-            data-is-production-environment="false"
-            src="https://snippet.meticulous.ai/v1/meticulous.js"
-          />
-        )}
-      </head>
       <body className="relative h-full w-full bg-zinc-950 min-h-screen">
         <div className="absolute -z-10 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
 
