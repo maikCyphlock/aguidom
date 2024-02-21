2f0fdb99e4613d9b1309072f9e34ae27e8d11bd0
maikCyphlock <maikolaguilar656@gmail.com>
2024-02-21 15:03:16 -0400

    ✅ feat: add testing

--- src/__tests__/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { render, screen, RenderResult } from "@testing-library/react";
-
-test("Page", async () => {
-
-
-  // expect(screen.findByText("Aguidom")).toBeDefined();
-});
